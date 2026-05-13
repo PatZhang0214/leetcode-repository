9a7997ec66c27a17510364e97f732857b1cc08b7
Patrick Zhang <patrick.zhang0502@gmail.com>
2026-05-13 17:29:23 -0400

    added more - May 13

--- easy/hashmap/HappyNumber.py
+++ easy/hashmap/HappyNumber.py
@@ -1,14 +1,17 @@
 def isHappy(n: int) -> bool:
-    while n ** 2 > 10:
+    st = set()
+    while True:
         n = str(n)
         total = 0
         for c in n:
             total += int(c) ** 2
         n = total
-    return n == 1
+        if n == 1:
+            return True
+        if n in st:
+            return False
+        print(n)
+        st.add(n)
 
 if __name__ == '__main__':
-    
-    # 1 is Happy
-    #
-    print(isHappy(4))
+    print(isHappy(19))
