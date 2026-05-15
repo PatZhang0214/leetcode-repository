0fc5adf5c29626b4952dceae96b0451cd4e9a854
Patrick Zhang <patrick.zhang0502@gmail.com>
2026-05-14 23:53:12 -0400

    Added more May 14

---
+++ easy/hashmap/ContainsDuplicate2.py
@@ -0,0 +1,12 @@
+def containsNearbyDuplicate(nums: List[int], k: int) -> bool:
+    window = set()
+    for i, num in enumerate(nums):
+        if num in window:
+            return True
+
+        window.add(num)
+
+        if len(window) > k:
+            window.remove(nums[i - k])
+
+    return False
